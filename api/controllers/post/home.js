759c0206e65134f93ce313362daa56e0a92e0365
Paul Defilippi <allinfo.pd@gmail.com>
2019-09-01 19:29:28 -0400

    added config settings for mongodb datastore

--- api/controllers/post/home.js
+++ api/controllers/post/home.js
@@ -2,7 +2,8 @@ module.exports = async function(req, res) {
     sails.log.warn("Show the post creation form now")
 
     const userId = req.session.userId
-    const allPosts = await Post.find({user: userId}).populate('user')
+    const allPosts = await Post.find({user: userId})
+        .populate('user')
         .sort('createdAt DESC')
 
     res.view('pages/post/home', {
